f3276a3af420f1eb044ad8e7a62be92b74210437
ViSta_ <777_Gaga@mail.ru>
2022-08-19 15:17:32 +0300

    Uploading the project

---
+++ FactoryMethodYouTubeUnity/Assets/Scripts/Archer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : Character
+{
+    public override void Attack()
+    {
+        Debug.Log("Shooting an arrow");
+    }
+}
---
+++ FactoryMethodYouTubeUnity/Assets/Scripts/Wizzard.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wizzard : Character
+{
+    public override void Attack()
+    {
+        Debug.Log("Casting a spell");
+    }
+}
